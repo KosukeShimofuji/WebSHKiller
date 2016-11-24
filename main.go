6a24519b99fb44e54814fd3485b04b3fa5b82b26
Kosuke Shimofuji <kosuke.shimofuji@gmail.com>
2016-11-24 10:36:00 +0900

    add debug function

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/fatih/color"
+	"github.com/kr/pretty"
 	"github.com/rackspace/gophercloud"
 	"github.com/rackspace/gophercloud/openstack"
+	"github.com/rackspace/gophercloud/openstack/compute/v2/flavors"
+	_ "github.com/rackspace/gophercloud/openstack/compute/v2/servers"
+	_ "github.com/rackspace/gophercloud/openstack/imageservice/v2/images"
+	"github.com/rackspace/gophercloud/pagination"
 	"os"
+	_ "reflect"
 )
 
 var (
@@ -14,6 +20,7 @@ var (
 	OPENSTACK_PASSWORD     = os.Getenv("OPENSTACK_PASSWORD")
 	OPENSTACK_TENANT_NAME  = os.Getenv("OPENSTACK_TENANT_NAME")
 	OPENSTACK_REGION       = os.Getenv("OPENSTACK_REGION")
+	DEBUG                  = true
 )
 
 func usage() {
@@ -22,11 +29,11 @@ func usage() {
 	c = color.New(color.FgYellow)
 	c.Printf("[ EXAMPLE ]\n")
 	c = color.New(color.FgCyan)
-	c.Printf("export OPENSTACK_IDENTITY_URI=\"https://identity.tyo1.conoha.io/v2.0\"\n")
+	c.Printf("export OPENSTACK_IDENTITY_URI=\"IDENTITY_URI\"\n")
 	c.Printf("export OPENSTACK_USERNAME=\"USERNAME\"\n")
 	c.Printf("export OPENSTACK_PASSWORD=\"PASSWORD\"\n")
 	c.Printf("export OPENSTACK_TENANT_NAME=\"TENANT_NAME\"\n")
-	c.Printf("export OPENSTACK_REGION=\"tyo1\"\n")
+	c.Printf("export OPENSTACK_REGION=\"REGION\"\n")
 }
 
 func crit(msg string) {
@@ -35,8 +42,29 @@ func crit(msg string) {
 	os.Exit(255)
 }
 
+func debug(msg string) {
+	c := color.New(color.FgGreen)
+	c.Printf("[DEBUG] %s\n", msg)
+}
+
+func view_index_flavor(client *gophercloud.ServiceClient) {
+	pager := flavors.ListDetail(client, nil)
+	pager.EachPage(func(page pagination.Page) (bool, error) {
+		flavorList, err := flavors.ExtractFlavors(page)
+		if err != nil {
+			fmt.Println(err)
+			return false, err
+		}
+		for _, f := range flavorList {
+			fmt.Printf("%# v\n", pretty.Formatter(f))
+		}
+		return true, nil
+	})
+}
+
 func main() {
 	// 環境変数が設定されていないならusageを出力して終了する
+	debug("Check identity information")
 	if OPENSTACK_IDENTITY_URI == "" ||
 		OPENSTACK_USERNAME == "" ||
 		OPENSTACK_PASSWORD == "" ||
@@ -47,17 +75,28 @@ func main() {
 	}
 
 	// 環境変数から受け取ったOpenStackの認証情報を用いて認証を実施する
-	opts := gophercloud.AuthOptions{
+	debug("Authentication to OpenStack")
+	openstack_opts := gophercloud.AuthOptions{
 		IdentityEndpoint: OPENSTACK_IDENTITY_URI,
 		Username:         OPENSTACK_USERNAME,
 		Password:         OPENSTACK_PASSWORD,
 		TenantName:       OPENSTACK_TENANT_NAME,
 	}
-	provider, err := openstack.AuthenticatedClient(opts)
+	provider, err := openstack.AuthenticatedClient(openstack_opts)
 	if err != nil {
 		fmt.Printf("OpenStack authentication error : %s", err.Error())
 	}
-	fmt.Println(provider)
 
-	// List all available flavors
+	// compute node clientを作成する
+	debug("Carete compute node client")
+	compute_client, err := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
+		Region: OPENSTACK_REGION,
+	})
+	if err != nil {
+		fmt.Printf("Create OpenStack instance error : %s", err.Error())
+	}
+
+	// flavorの一覧表示を行う
+	debug("Get a list of flavor")
+	view_index_flavor(compute_client)
 }
