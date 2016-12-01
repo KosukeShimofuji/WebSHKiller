e9c5f429e6db4085241d7dd81a8f34527bd394de
Kosuke Shimofuji <kosuke.shimofuji@gmail.com>
2016-12-01 20:17:21 +0900

    change prefix at Crit function

--- logger/logger.go
+++ logger/logger.go
@@ -27,7 +27,7 @@ func Crit(s string) {
 	c := color.New(color.FgGreen)
 	ts := time.Now().Format("2006/01/02 15:04:05 MST")
 	_, filename, line_no, _ := runtime.Caller(1)
-	c.Printf("[DEBUG][%s][%s:%d] %s\n",
+	c.Printf("[CRITICAL][%s][%s:%d] %s\n",
 		ts,
 		filepath.Base(filename),
 		line_no,
